d56ab3a4dafa7966bbbf61e31d5d18350a2b55e4
Prakash Shanmugam <mail.prakash.india@gmail.com>
2018-05-01 16:04:20 +0530

    added update and delete topic using repository

--- src/main/java/com/example/springbootstarter/topic/TopicService.java
+++ src/main/java/com/example/springbootstarter/topic/TopicService.java
@@ -1,7 +1,6 @@
 package com.example.springbootstarter.topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +12,6 @@ public class TopicService {
 	@Autowired
 	private TopicRepository topicRepository;
 
-	private List<Topic> topics = new ArrayList<>(
-			Arrays.asList(new Topic("spring", "Spring framework", "Spring framework description"),
-					new Topic("java", "Core Java", "Core Java description"),
-					new Topic("javascript", "Javascript", "Javascript description")));
-
 	public List<Topic> getTopics() {
 		List<Topic> topics = new ArrayList<>();
 		topicRepository.findAll().forEach(topics::add);
@@ -26,7 +20,7 @@ public class TopicService {
 	}
 
 	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findOne(id);
 	}
 
 	public void addTopic(Topic topic) {
@@ -34,17 +28,10 @@ public class TopicService {
 	}
 
 	public void updateTopic(String id, Topic topic) {
-		for (int i = 0; i < topics.size(); i++) {
-			Topic t = topics.get(i);
-
-			if (t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
+		topicRepository.save(topic);		
 	}
 
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.delete(id);
 	}
 }
