67ee77b7ed3bd520d77914c73cca9da0a5e0f3ed
Prakash Shanmugam <mail.prakash.india@gmail.com>
2018-05-01 15:36:34 +0530

    added topic repository and add method

--- src/main/java/com/example/springbootstarter/topic/TopicService.java
+++ src/main/java/com/example/springbootstarter/topic/TopicService.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
+
+	@Autowired
+	private TopicRepository topicRepository;
 
 	private List<Topic> topics = new ArrayList<>(
 			Arrays.asList(new Topic("spring", "Spring framework", "Spring framework description"),
@@ -15,6 +19,9 @@ public class TopicService {
 					new Topic("javascript", "Javascript", "Javascript description")));
 
 	public List<Topic> getTopics() {
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
+
 		return topics;
 	}
 
@@ -23,7 +30,7 @@ public class TopicService {
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
 
 	public void updateTopic(String id, Topic topic) {
